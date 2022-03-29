614dd4035ace8c9269b28c301d5dae325fb4b049
Amin <amin.sanei81@gmail.com>
2022-03-29 12:25:58 +0430

    js became object oriented

--- main.js
+++ main.js
@@ -1,84 +1,81 @@
+//__________________________________________________classes__________________________________________________
+class Navbar {
+    //// private:
+    #isToggleOpen;
+    #delayTime;
+    #hamburgerMenuBottom() {
+        window.clearTimeout(this.#delayTime);
+        if (this.#isToggleOpen == 1) {
+            $(".center").removeClass("rotate");
+            this.#delayTime = window.setTimeout(() => {
+                $(".center").removeClass("open");
+            }, 160);
+        } else {
+            $(".center").addClass("open");
+            this.#delayTime = window.setTimeout(() => {
+                $(".center").addClass("rotate");
+            }, 160);
+        }
+    }
+    #hamburgerMenuStyle() {
+        if (this.#isToggleOpen == 1) {
+            $("#hamburger-menu").slideUp(400, "linear");
+            setTimeout(function () {
+                $(".tablet").removeAttr("style");
+                $(".tablet li").last().animate(
+                    {
+                        top: 0,
+                        opacity: 1,
+                    },
+                    250
+                );
+                $("html").css("overflow", "visible");
+            }, 300);
+        } else {
+            $("html").css("overflow", "hidden");
+            $(".tablet").css("background-color", "#000");
+            $(".tablet li").last().animate(
+                {
+                    top: 3,
+                    opacity: 0,
+                },
+                300
+            );
+            $("#hamburger-menu").slideDown(400, "linear");
+        }
+    }
+
+    //// public:
+    constructor() {
+        this.#isToggleOpen = -1;
+    }
+    hamburgerMenu() {
+        this.#hamburgerMenuBottom();
+        this.#hamburgerMenuStyle();
+        this.#isToggleOpen *= -1;
+    }
+    responsive() {
+        var width = $(window).width();
+        if (width <= 832) {
+            $("#navbar").hide();
+            $(".tablet").show();
+        }
+        if (width > 832) {
+            $("#navbar").show();
+            $(".tablet").hide();
+        }
+    }
+}
+//__________________________________________________main()___________________________________________________
 $(document).ready(function () {
-    responsive();
-    $(window).on('resize', responsive);
-
-
-    let isToggleOpen = -1; // -1 === false
-    let delayTime;
-    $(".frame").on('click', () => {
-        hamburgerMenuBottom(isToggleOpen, delayTime);
-        hamburgerMenuStyle(isToggleOpen);
-        isToggleOpen *= -1;
+    const nav = new Navbar();
+    // responsive
+    nav.responsive();
+    $(window).on("resize", () => {
+        nav.responsive();
+    });
+    //hamburgerMenu
+    $(".frame").on("click", () => {
+        nav.hamburgerMenu();
     });
 });
-//-------------------------------------------------------------------
-//                              Responsive
-//-------------------------------------------------------------------
-function responsive() {
-    // -----------------navbar-----------------
-    var width = $(window).width();
-    if (width <= 832) {
-        $("#navbar").hide();
-        $(".tablet").show();
-    }
-    if (width > 832) {
-        $("#navbar").show();
-        $(".tablet").hide();
-    }
-    // -----------------other-----------------
-}
-//-------------------------------------------------------------------
-//                              hamburgerMenuBottom & style
-//-------------------------------------------------------------------
-function hamburgerMenuBottom(isToggle, delay) {
-    window.clearTimeout(delay);
-    if (isToggle == 1) {
-        $(".center").removeClass("rotate");
-        delay = window.setTimeout(() => {
-            $(".center").removeClass("open");
-        }, 160);
-
-    } else {
-        $(".center").addClass("open");
-        delay = window.setTimeout(() => {
-            $(".center").addClass("rotate");
-        }, 160);
-
-    }
-}
-
-function hamburgerMenuStyle(isToggle) {
-
-    if (isToggle == 1) {
-        $("#hamburger-menu").slideUp(400,"linear");
-        setTimeout(function () {
-            $(".tablet").removeAttr("style");
-            $(".tablet li").last().animate({
-                top: 0,
-                opacity: 1
-            }, 250);
-            $("html").css("overflow", "visible");
-        }, 300)
-    } else {
-        $("html").css("overflow", "hidden");
-        $(".tablet").css("background-color", "#000");
-        $(".tablet li").last().animate({
-            top: 3,
-            opacity: 0
-        }, 300);
-        $("#hamburger-menu").slideDown(400,"linear");
-    }
-}
-//-------------------------------------------------------------------
-// 
-//-------------------------------------------------------------------
-
-
-//-------------------------------------------------------------------
-// 
-//-------------------------------------------------------------------
-
-
-//-------------------------------------------------------------------
-// 
-//-------------------------------------------------------------------
